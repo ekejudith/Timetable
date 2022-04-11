792b06fec631862fee11e73f852e4e82374b1ec9
Judith Siegel <siegeljudith12@gmail.com>
2022-04-11 20:42:44 +0300

    Lab2_javitva

--- Form/form.js
+++ Form/form.js
@@ -18,10 +18,10 @@ function emailValidator() {
 }
 
 function webpageValidator() {
-    let regex = /^([a-zA-Z0-9_-]+).([a-zA-Z0-9_-]+).([a-zA-Z0-9_-]+)/;
-
+    let regex = /(.*\/\/){0,1}[a-zA-Z0-9-]+\.[A-Za-z0-9-]+\.[A-Za-z0-9-]{2,63}(\/.*)?/;
     let webpage = document.getElementById("webpage").value;
-    if (webpage === "" || !regex.test(webpage)) {
+    let isValid = myForm.elements[3].validity.valid;
+    if (webpage === "" || !regex.test(webpage) || !isValid) {
         document.getElementById("webpageValidatorLabel").removeAttribute("hidden");
         return false;
     }
@@ -31,12 +31,26 @@ function webpageValidator() {
     }
 }
 
+function nameValidator(){
+    let name = myForm.elements[0].validity.valid;
+    if (name==false){
+        document.getElementById("nameValidatorLabel").removeAttribute("hidden");
+        return false;
+    }
+    else {
+        document.getElementById("nameValidatorLabel").setAttribute("hidden", "hidden");
+        return true;
+    }
+}
+
 
 const myForm = document.getElementById("form");
+let input = document.querySelectorAll('input');
+input.forEach(function(elem){ elem.addEventListener('change', checkInputs);});
 
-myForm.addEventListener('submit', function (event) {
-
-    let name = myForm.elements[0].validity.valid;
+function checkInputs() {
+    let button = document.getElementById("submit");
+    let name = nameValidator() && myForm.elements[0].validity.valid;
     let date = myForm.elements[1].validity.valid;
     let webpage = webpageValidator() && myForm.elements[2].validity.valid;
     let email = emailValidator() && myForm.elements[3].validity.valid;
@@ -50,11 +64,12 @@ myForm.addEventListener('submit', function (event) {
         sessionStorage.setItem("myName", myName);
         sessionStorage.setItem("myNickname", myNickname);
         sessionStorage.setItem("myMoney", myMoney);
+
+        button.disabled = false;
+    } else {
+        button.disabled = true;
     }
-    else {
-        event.preventDefault();
-    }
-});
+}
 
 function footer() {
     document.getElementsByClassName("footer")[0].getElementsByTagName("div")[0].innerHTML = document.lastModified;
